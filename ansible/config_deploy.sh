dc85aefae47d8cdbe4a00e69c41490869944c476
alebeta <alebeta@gonkar.com>
2019-01-11 17:13:24 +0100

    first changes

--- ansible/config_deploy.sh
+++ ansible/config_deploy.sh
@@ -9,9 +9,6 @@ Performs a config deploy to the Habitat cluster to update configurations for the
   exit 1
 fi
 
-# TODO this should get a lot smarter -- if you are on a branch, disallow. If you have working changes, warn the user and require a flag.
-git pull origin master
-
 HOST_TYPE=$1
 ENVIRONMENT=$2
 TARGET_IP=$3
@@ -19,4 +16,4 @@ TARGET_IP=$3
 [[ -z "$ENVIRONMENT" ]] && ENVIRONMENT=dev
 
 
-ansible-playbook --ask-vault-pass -i "${TARGET_IP}," -u ubuntu "${HOST_TYPE}-config.yml"
+ansible-playbook -i "${TARGET_IP}," -u ubuntu "${HOST_TYPE}-config.yml"
